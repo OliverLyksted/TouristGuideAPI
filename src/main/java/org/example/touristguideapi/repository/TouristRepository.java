9d57c4800a05cfe57de8dfbc205fdeae4549f4d3
OliverLyksted <olly0001@stud.kea.dk>
2024-02-05 18:10:52 +0100

    har oprettet et par attraktioner

--- src/main/java/org/example/touristguideapi/repository/TouristRepository.java
+++ src/main/java/org/example/touristguideapi/repository/TouristRepository.java
@@ -1,8 +1,24 @@
 package org.example.touristguideapi.repository;
 
 
+import org.example.touristguideapi.model.TouristAttraction;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class TouristRepository {
+    private List<TouristAttraction> touristAttractions;
+
+    public TouristRepository(){
+        touristAttractions = new ArrayList<>();
+        touristAttractions.add(new TouristAttraction("Zoo", "A place where you can see many different animals"));
+        touristAttractions.add(new TouristAttraction("Tivoli", "A very popular Amusement park"));
+    }
+
+    public List<TouristAttraction> getTouristAttractions(){
+        return touristAttractions;
+    }
+
 }
