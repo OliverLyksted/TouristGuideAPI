0fda7bc09658638dc17b88504c768fd0efef3370
ThomasFBN <thni0004@stud.kea.dk>
2024-02-27 12:21:06 +0100

    test

--- src/test/java/org/example/touristguideapi/TouristControllerTest.java
+++ src/test/java/org/example/touristguideapi/TouristControllerTest.java
@@ -3,16 +3,19 @@ package org.example.touristguideapi;
 import org.example.touristguideapi.controller.TouristController;
 import org.example.touristguideapi.model.TouristAttraction;
 import org.example.touristguideapi.service.TouristService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+
 import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @WebMvcTest(TouristController.class)
 class TouristControllerTest {
@@ -30,13 +33,33 @@ class TouristControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("attractionList"));
     }
+
     @Test
-    void showTags() throws Exception{
-        mockMvc.perform(get("/attractions/SMK/tags"))
-                .andExpect(status().isOk()).
-                andExpect(view().name("tags")).
-                andExpect(content().string(containsString("SMK tags")));
+    void showTags() throws Exception {
+        mockMvc.perform(get("/attractions/Zoo/tags"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("tags"))
+                .andExpect(content().string(containsString("Tags for Zoo")));
     }
+
+    @Test
+    void createTouristAttractionForm() throws Exception {
+        mockMvc.perform(get("/attractions/add"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("cities", Matchers.empty()))
+                .andExpect(model().attribute("tags", Matchers.empty()))
+                .andExpect(model().attribute("touristAttraction", Matchers.any(TouristAttraction.class)))
+                .andExpect(view().name("addAttraction"));
+    }
+
+
+    @Test
+    void saveTouristAttraction() throws Exception {
+        mockMvc.perform(post("/attractions/save").sessionAttr("touristAttraction", this.touristAttraction))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/attractions"));
+    }
+
 }
 
 
