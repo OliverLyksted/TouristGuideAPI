52451d16a40da14b002c55049701d333aadaa81c
OliverLyksted <144331612+OliverLyksted@users.noreply.github.com>
2024-03-19 14:05:36 +0100

    update og delete

--- src/main/java/org/example/touristguideapi/repository/TouristRepository.java
+++ src/main/java/org/example/touristguideapi/repository/TouristRepository.java
@@ -18,8 +18,8 @@ public class TouristRepository {
         touristAttractions.add(new TouristAttraction(1,"Zoo", "The Zoo in Copenhagen is one of the oldest culture institutions in Denmark. The origin of the Zoo dates all the way back to 1859 where it was one of the first zoo's in all of Europe! Today the Zoo is one of the most attractive places for a fun and educational day with either family, friends or even alone. The Zoo has around 1.3 million guests every year, and with more than 4000 different animals and 250 different sorts of animals, it is impossible not to have fun when visiting", "København", List.of("Underholdning", "Familie", "Restaurant")));
         touristAttractions.add(new TouristAttraction(2,"Tivoli", "A very popular Amusement park", "København", List.of("Underholdning", "Restaurant", "Koncert")));
     }
-    /*
 
+/*
     public List<TouristAttraction> getTouristAttractions() {
         return touristAttractions;
     }
@@ -90,7 +90,7 @@ public class TouristRepository {
     }
 */
 
-    public TouristAttraction getAttractionByName(String name) throws SQLException {
+    public TouristAttraction getAttractByName(String name) throws SQLException {
         String sql = "SELECT * FROM TouristAttraction WHERE name = ?";
         try (Connection connection = DriverManager.getConnection("jdbc:mysql://turistguidedb.mysql.database.azure.com/touristguide_db", "touristguideTEO", "Database1")) {
             try (PreparedStatement statement = connection.prepareStatement(sql)) {
@@ -144,6 +144,32 @@ public class TouristRepository {
             }
         }
         return city;
+    }
+    public void deleteAttractions(String name) throws SQLException {
+        String sql = "DELETE FROM TouristAttraction WHERE name = ?";
+        try ( Connection connection = DriverManager.getConnection("jdbc:mysql://turistguidedb.mysql.database.azure.com/touristguide_db", "touristguideTEO", "Database1")){
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1,name);
+                int rowsAffected = statement.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void updateAttrations(TouristAttraction updatedAttraction) throws SQLException {
+        String sql = "UPDATE TouristAttraction SET description = ?, tags = ?, city = ? WHERE name = ?";
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://turistguidedb.mysql.database.azure.com/touristguide_db", "touristguideTEO", "Database1")) {
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, updatedAttraction.getDescription());
+                statement.setString(2, String.join(",", updatedAttraction.getTags()));
+                statement.setString(3, updatedAttraction.getCity());
+                statement.setString(4, updatedAttraction.getName());
+                int rowsAffected = statement.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
